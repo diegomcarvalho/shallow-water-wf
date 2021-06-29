99c1a97213884b170833838efb4c496c7c3956a1
Diego Carvalho <d.carvalho@ieee.org>
2021-06-29 17:46:50 -0300

    FileFuture & FileFutureFactory

--- kf_workflow.py
+++ kf_workflow.py
@@ -1,3 +1,4 @@
+import os.path
 from apps import KFS2d, model_normalize, mpca_ann
 import parsl
 from parsl.configs.local_threads import config
@@ -5,35 +6,89 @@ from parsl.data_provider.files import File
 
 parsl.load(config)
 
-KF = KFS2d("1 10 10 100 10 2 2 0.1 10",
-           outputs=[
-               parsl.File(
-                   "kfs2d_rna_mirror/output/training/qAnalysisExpA.out"),
-               parsl.File("kfs2d_rna_mirror/output/training/qModelExpA.out"),
-               parsl.File("kfs2d_rna_mirror/output/training/qObservExpA.out")
-           ])
 
-KF.result()
+class FileFuture(object):
+    '''docstring for FileFuture.'''
 
-MN = model_normalize(
-    inputs=[
-        KF.outputs[0],
-        KF.outputs[1],
-        KF.outputs[2]],
-    outputs=[
-        parsl.File("octave/x.txt"),
-        parsl.File("octave/x_gen.txt"),
-        parsl.File("octave/x_valid.txt"),
-        parsl.File("octave/y.txt"),
-        parsl.File("octave/y_gen.txt"),
-        parsl.File("octave/y_valid.txt")
-    ])
+    def __init__(self, file_name):
+        self._full = os.path.abspath(file_name)
+        self._name = os.path.basename(self._full)
+        self._path = os.path.dirname(self._full)
+        self._future = File(self._full)
+        return
 
-MN.result()
+    @property
+    def name(self):
+        return self._name
 
-mpca_status = mpca_ann(inputs=MN.outputs, outputs=[
-                       parsl.File("mpca-ann/output/ann1.best")])
+    @property
+    def path(self):
+        return self._path
 
-with open(mpca_status.outputs[0].result(), 'r') as f:
-    with open("ann1.best", "w") as w:
-        w.write(f.read())
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def future(self):
+        return self._future
+
+
+class FileFutureFactory(object):
+    '''docstring for FileFutureFactory.'''
+
+    def __init__(self, *argv):
+        self._file_list = list()
+        for arg in argv:
+            self._file_list.append(FileFuture(arg))
+        return
+
+    @property
+    def file_list(self):
+        return self._file_list
+
+    @property
+    def future_list(self):
+        return [f.future for f in self._file_list]
+
+    def result(self):
+        for f in self._file_list:
+            f.future.result()
+        return
+
+
+if __name__ == '__main__':
+
+    print("KF Workflow")
+    KFS2d_output = FileFutureFactory(
+        'kfs2d_rna_mirror/output/training/qAnalysisExpA.out',
+        'kfs2d_rna_mirror/output/training/qModelExpA.out',
+        'kfs2d_rna_mirror/output/training/qObservExpA.out'
+    )
+
+    octave_output = FileFutureFactory(
+        'octave/x.txt',
+        'octave/x_gen.txt',
+        'octave/x_valid.txt',
+        'octave/y.txt',
+        'octave/y_gen.txt',
+        'octave/y_valid.txt'
+    )
+
+    KF = KFS2d('1 10 10 100 10 2 2 0.1 10', outputs=KFS2d_output.future_list)
+
+    KF.result()
+
+    MN = model_normalize(
+        inputs=[x for x in KF.outputs],
+        outputs=octave_output.future_list
+    )
+
+    MN.result()
+
+    mpca_status = mpca_ann(inputs=MN.outputs, outputs=[
+        parsl.File('mpca-ann/output/ann1.best')])
+
+    with open(mpca_status.outputs[0].result(), 'r') as f:
+        with open('ann1.best', 'w') as w:
+            w.write(f.read())
